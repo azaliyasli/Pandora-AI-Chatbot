1f9a5198459508c293dbc886bcfb9067a048f09a
Aslı Akın <slkn521@gmail.com>
2026-01-23 13:21:37 +0300

    Training added

--- nlp.py
+++ nlp.py
@@ -3,8 +3,10 @@ import json
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from torch.utils.data import TensorDataset, DataLoader
-from transformers import DistilBertTokenizer
+from tqdm import tqdm
+from transformers import DistilBertTokenizer, DistilBertForSequenceClassification, get_linear_schedule_with_warmup
 import torch
+from torch.optim import AdamW
 
 # Data Preparation
 with open('intents.json', 'r', encoding='utf-8') as f:
@@ -70,3 +72,59 @@ test_dataset = TensorDataset(
 
 train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+
+# Training
+device = torch.device("cpu")
+
+model = DistilBertForSequenceClassification.from_pretrained(
+    "distilbert-base-uncased",
+    num_labels=len(le.classes_)
+)
+model.to(device)
+
+optimizer = AdamW(model.parameters(), lr=5e-5)
+epochs = 10
+total_steps = len(train_loader) * epochs
+
+scheduler = get_linear_schedule_with_warmup(
+    optimizer,
+    num_warmup_steps=0,
+    num_training_steps=total_steps
+) # Slow the learning process down for higher accuracy
+
+for epoch in range(epochs):
+    model.train()
+    total_train_loss = 0
+
+    loop = tqdm(train_loader, leave=True)
+
+    for batch in loop:
+        b_input_ids = batch[0].to(device)
+        b_input_mask = batch[1].to(device)
+        b_labels = batch[2].to(device)
+
+        model.zero_grad()
+
+        outputs = model(
+            b_input_ids,
+            attention_mask=b_input_mask,
+            labels=b_labels
+        )
+
+        loss = outputs.loss
+        total_train_loss += loss.item()
+
+        loss.backward()
+
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+
+        optimizer.step()
+        scheduler.step()
+
+        loop.set_description(f"Epoch {epoch + 1}")
+        loop.set_postfix(loss=loss.item())
+
+    avg_train_loss = total_train_loss / len(train_loader)
+    print(f"Epoch {epoch + 1}/{epochs} - Average Loss: {avg_train_loss:.4f}")
+
+torch.save(model.state_dict(), "distilbert_pandora.pt") # For RAG and FastAPI
